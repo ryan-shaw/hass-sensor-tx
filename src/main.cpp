c73b2338f2998f2c38bb30b2560e47363c86339d
Ryan Shaw <ryan.shaw@min.vc>
2017-11-18 00:07:29 +0000

    Working transmit

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,71 @@
-// DHT Temperature & Humidity Sensor
-// Unified Sensor Library Example
-// Written by Tony DiCola for Adafruit Industries
-// Released under an MIT license.
 
-// Depends on the following Arduino libraries:
-// - Adafruit Unified Sensor Library: https://github.com/adafruit/Adafruit_Sensor
-// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
+/** RF24Mesh_Example.ino by TMRh20
 
+   This example sketch shows how to manually configure a node via RF24Mesh, and send data to the
+   master node.
+   The nodes will refresh their network address as soon as a single write fails. This allows the
+   nodes to change position in relation to each other and the master node.
+*/
+
+
+#include "RF24.h"
+#include "RF24Network.h"
+#include "RF24Mesh.h"
+#include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
-#include <VirtualWire.h>
+#include <LowPower.h>
+//#include <printf.h>
 
-#define DHTPIN            2         // Pin which is connected to the DHT sensor.
-#define TXPIN             12
-#define LEDPIN            8
+#define TEMP 0
+#define HUMIDITY 1
 
-// Uncomment the type of sensor in use:
-#define DHTTYPE           DHT11     // DHT 11 
-// #define DHTTYPE           DHT22     // DHT 22 (AM2302)
-//#define DHTTYPE           DHT21     // DHT 21 (AM2301)
 
-// See guide for details on sensor wiring and usage:
-//   https://learn.adafruit.com/dht/overview
-
+/**** Configure DHT11 */
+#define DHTPIN 2
+#define DHTTYPE DHT11     // DHT 11 
 DHT_Unified dht(DHTPIN, DHTTYPE);
 
-char *controller;
-uint32_t delayMS;
+/**** Configure the nrf24l01 CE and CS pins ****/
+RF24 radio(7, 8);
+RF24Network network(radio);
+RF24Mesh mesh(radio, network);
+
+/**
+   User Configuration: nodeID - A unique identifier for each radio. Allows addressing
+   to change dynamically with physical changes to the mesh.
+
+   In this example, configuration takes place below, prior to uploading the sketch to the device
+   A unique value from 1-255 must be configured for each node.
+   This will be stored in EEPROM on AVR devices, so remains persistent between further uploads, loss of power, etc.
+
+ **/
+#define nodeID 1
+
+
+uint32_t displayTimer = 0;
+
+struct payload_t {
+  unsigned long ms;
+  unsigned long counter;
+};
+
+struct dataStruct{
+  uint16_t sensor;
+  uint16_t type;
+  float value;
+};
+
+dataStruct myData;
 
 void setup() {
-  Serial.begin(9600); 
 
-  // Init TX module
-  vw_set_ptt_inverted(true); //
-  vw_set_tx_pin(TXPIN);
-  vw_setup(500);// speed of data transfer Kbps
-  pinMode(LEDPIN, OUTPUT);
+  Serial.begin(115200);
 
-  // Initialize device.
+  // Setup  DHT11 sensor
   dht.begin();
-  Serial.println("DHTxx Unified Sensor Example");
+  // Serial.println("DHTxx Unified Sensor Example");
   // Print temperature sensor details.
   sensor_t sensor;
   dht.temperature().getSensor(&sensor);
@@ -65,42 +90,80 @@ void setup() {
   Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println("%");  
   Serial.println("------------------------------------");
   // Set delay between sensor readings based on sensor details.
-  delayMS = sensor.min_delay / 1000;
+
+  //printf_begin();
+  // Set the nodeID manually
+  mesh.setNodeID(nodeID);
+  // Connect to the mesh
+  Serial.println(F("Connecting to the mesh..."));
+  mesh.begin();
+}
+
+void send(struct dataStruct msg) {
+  // Send an 'M' type message containing the current millis()
+  if (!mesh.write((uint8_t*) &msg, 'M', sizeof(msg))) {
+    // If a write fails, check connectivity to the mesh network
+    if ( ! mesh.checkConnection() ) {
+      //refresh the network address
+      Serial.println("Renewing Address");
+      mesh.renewAddress();
+    } else {
+      Serial.println("Send fail, Test OK");
+    }
+  } else {
+    Serial.print("Send OK: "); Serial.println(displayTimer);
+  }
 }
 
 void loop() {
-  // Delay between measurements.
-  delay(delayMS);
-  // Get temperature event and print its value.
-  sensors_event_t event;  
-  dht.temperature().getEvent(&event);
-  if (isnan(event.temperature)) {
-    Serial.println("Error reading temperature!");
+
+  mesh.update();
+
+  // Send to the master node every second
+  if (millis() - displayTimer >= 5 * 60 * 1000) {
+    displayTimer = millis();
+
+    sensors_event_t event;  
+    dht.temperature().getEvent(&event);
+    if (isnan(event.temperature)) {
+      Serial.println("Error reading temperature!");
+    } else {
+      Serial.print("Temperature: ");
+      Serial.print(event.temperature);
+      Serial.println(" *C");
+      
+      // dataStruct myData;
+      myData.sensor = nodeID;
+      myData.type = TEMP;
+      myData.value = event.temperature;
+      send(myData);
+    }
+
+    // Get humidity event and print its value.
+    dht.humidity().getEvent(&event);
+    if (isnan(event.relative_humidity)) {
+      // Serial.println("Error reading humidity!");
+    } else {
+      Serial.print("Humidity: ");
+      Serial.print(event.relative_humidity);
+      Serial.println("%");
+
+      // dataStruct myData;
+      myData.sensor = nodeID;
+      myData.type = HUMIDITY;
+      myData.value = event.relative_humidity;
+      send(myData);
+    }
+
   }
-  else {
-    Serial.print("Temperature: ");
-    Serial.print(event.temperature);
-    Serial.println(" *C");
-    // Write temp to TX module
-    // sprintf(controller, "%.1f", event.temperature);;
-    int i = (int) event.temperature;
-    char msg[4];
-    itoa(i,msg,10);
-    digitalWrite(LEDPIN, HIGH);
-    Serial.println(msg);
-    vw_send((uint8_t *)msg, strlen(msg));
-    vw_wait_tx(); // Wait until the whole message is gone
-    delay(1000);
-    digitalWrite(LEDPIN, LOW);
-  }
-  // Get humidity event and print its value.
-  dht.humidity().getEvent(&event);
-  if (isnan(event.relative_humidity)) {
-    Serial.println("Error reading humidity!");
-  }
-  else {
-    Serial.print("Humidity: ");
-    Serial.print(event.relative_humidity);
-    Serial.println("%");
+
+  while (network.available()) {
+    RF24NetworkHeader header;
+    payload_t payload;
+    network.read(header, &payload, sizeof(payload));
+    Serial.print("Received packet #");
+    Serial.print(payload.counter);
+    Serial.print(" at ");
+    Serial.println(payload.ms);
   }
 }
